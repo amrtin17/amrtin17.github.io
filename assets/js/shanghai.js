ff71b8bac9e7c231d3ac29d38297158ee4232b4c
amrtin17 <amrtin@163.com>
2026-07-29 08:49:02 +0800

    Remove loadData function from shanghai.js
    Removed loadData function and its invocation.

--- assets/js/shanghai.js
+++ assets/js/shanghai.js
@@ -1,19 +1,4 @@
 /* ==========================================
    Load CSV Data
 ========================================== */
-
-async function loadData() {
-
-    console.log("Start loading...");
-
-    const response = await fetch("assets/data/poi.csv");
-
-    console.log(response);
-
-    const csv = await response.text();
-
-    console.log(csv);
-
-}
-
-loadData();
+alert("JS Loaded");
